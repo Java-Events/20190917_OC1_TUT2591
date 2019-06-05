e104f437f70a6aa7d7ce7a4c94de28318c206a5d
Sven Ruppert <sven.ruppert@gmail.com>
2019-06-05 12:42:11 +0200

    NanoEngine - discovery is running

--- src/test/java/junit/org/rapidpm/junit/engine/nano/SecondNanoTest.java
+++ src/test/java/junit/org/rapidpm/junit/engine/nano/SecondNanoTest.java
@@ -4,19 +4,9 @@ import org.rapidpm.junit.engine.nano.NanoTest;
 import org.rapidpm.junit.engine.nano.NanoTestClass;
 
 @NanoTestClass
-public class FirstNanoTest {
+public class SecondNanoTest {
 
   @NanoTest
-  void test001_A() {
-
-  }
-
-  @NanoTest
-  void test001_B() {
-
-  }
-
-  //@NanoTest
   void test002() {
 
   }
